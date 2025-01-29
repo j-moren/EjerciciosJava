4f08ebc1dcde2ea82aa6fe93ec948eed61d6ee5b
J-Moren <kalev.1503@gmail.com>
2025-01-29 00:17:23 -0500

    Agregar Ejercicio1

---
+++ src/Ejercicio1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Ejercicio1 {
+    public static void main(String[] args){
+//Declaración de Variables
+int n1, n2;
+
+Scanner sc = new Scanner(System.in);
+//Leer el Primer Número
+System.out.println("Introduce un Número Entero: ");
+n1 = sc.nextInt(); //Lee un Entero por Teclado
+//Leer el segundo Número
+System.out.println("Introduce otro Número Entero: ");
+n2 = sc.nextInt(); //Lee un Entero por Teclado
+//Mostrar Resultado
+System.out.println("Ha Introducido los Números: "+ n1 +" y " + n2);
+
+    }
+        
+}
