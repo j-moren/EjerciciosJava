7d86a0e8aea16093a2d0f6bb8c991eb41429a161
J-Moren <kalev.1503@gmail.com>
2025-01-29 00:46:19 -0500

    Se Agrega actualización Ejercicio2

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,5 +1,15 @@
+//Programa Java que lea un nombre y muestre por pantalla: "Buenos días nombre_Introducido"
+
+import java.util.*;
 public class Ejercicio2 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Declaración Variables
+        Scanner sc = new Scanner(System.in); 
+        String cadena;
+        //Leer Nombre Introducido
+        System.out.println("Introduce un Nombre: ");
+        cadena = sc.nextLine();
+        //Mostrar Resultado
+        System.out.println("Buenos Dias " + cadena);
     }
 }
